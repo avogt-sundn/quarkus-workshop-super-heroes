4cd7969b2b83b179ebc851d82d6dc8f34d5312af
Armin Vogt <armin.vogt@sn-invent.de>
2022-09-27 17:11:55 +0200

    as taken from github

---
+++ super-heroes/extension-version/deployment/target/generated-sources/annotations/io/quarkus/workshop/superheroes/version/deployment/VersionConfig$$accessor.java
@@ -0,0 +1,12 @@
+package io.quarkus.workshop.superheroes.version.deployment;
+public final class VersionConfig$$accessor {
+    private VersionConfig$$accessor() {}
+    @SuppressWarnings("unchecked")
+    public static boolean get_enabled(Object __instance) {
+        return ((VersionConfig)__instance).enabled;
+    }
+    @SuppressWarnings("unchecked")
+    public static void set_enabled(Object __instance, boolean enabled) {
+        ((VersionConfig)__instance).enabled = enabled;
+    }
+}
