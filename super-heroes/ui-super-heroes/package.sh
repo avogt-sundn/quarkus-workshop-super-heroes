58e9d50b96cf7a5f2b19f97ccff5d74fe01ed4c2
Armin Vogt <armin.vogt@sn-invent.de>
2022-11-08 16:02:35 +0000

    batch-1

--- super-heroes/ui-super-heroes/package.sh
+++ super-heroes/ui-super-heroes/package.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # tag::adocShell[]
+export NODE_OPTIONS=--openssl-legacy-provider; 
 ./node_modules/.bin/ng build --configuration production --base-href "."
 # end::adocShell[]
 
